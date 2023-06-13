038bfd4952944ad57520c6b8cc8352c1069771c9
alejandroMilos <alejandromilospersonal@gmail.com>
2023-06-12 18:21:33 -0600

    Modif main

--- system.cpp
+++ system.cpp
@@ -3,7 +3,7 @@
 // Situacion Problema: POO - Consulta Alimenticia 
 
 #include <iostream>
-#include "FoodGroup.h"
+#include "FoodCatalog.h"
 #include "User.h"
 
 int main()
@@ -33,6 +33,7 @@ int main()
     std::cout << "\n" << std::endl;
 
     FoodGroup foodGroup;
+    FoodCatalog foodCatalog;
     while (true)
     {
         std::cout << "Do you wish to add food to an existing group or finalize the selection?" << std::endl;
@@ -52,7 +53,40 @@ int main()
             continue;
         }
 
-        foodGroup.setGroup(choice);
+        std::cout << "Food Catalog:" << std::endl;
+        std::cout << "1. Add a group" << std::endl;
+        std::cout << "2. Remove a group" << std::endl;
+        std::cout << "3. Show available groups" << std::endl;
+        std::cout << "Enter an option: ";
+        int catalogChoice;
+        std::cin >> catalogChoice;
+        std::cout << std::endl;
+
+        if (catalogChoice == 1)
+        {
+            std::cout << "Enter the name of the group to add: ";
+            std::string groupName;
+            std::cin >> groupName;
+            foodCatalog.addGroup(groupName);
+            std::cout << "----Group added successfully!----" << std::endl;
+            std::cout << std::endl;
+            foodGroup.setGroup(choice);
+        } else if (catalogChoice == 2)
+        {
+            std::cout << "Enter the name of the group to remove: ";
+            std::string groupName;
+            std::cin >> groupName;
+            foodCatalog.deleteGroup(groupName);
+            std::cout << "----Group removed successfully!----" << std::endl;
+        } else if (catalogChoice == 3)
+        {
+            foodCatalog.printGroupList();
+        } else
+        {
+            std::cout << "Invalid option! Please choose from the available ones." << std::endl;
+        }
+
+        std::cout << std::endl;
     }
 
     return 0;
