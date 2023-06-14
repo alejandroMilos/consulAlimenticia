e877b623e9b4ab09e47a10ddee45e04eca3f102e
MarcoMilos <marcomiloslavich03@gmail.com>
2023-06-13 19:40:38 -0600

    Modif printMenu

--- Menu.h
+++ Menu.h
@@ -7,9 +7,12 @@
 #include <list>
 #include <ctime> // se usa para trabajar con el tiempo y la fecha 
 #include "FoodCatalog.h"
+#include "User.h"
 
 class Menu
 {
+    FoodCatalog foodCatalog;
+    User user;
     int quantity;
     double totalCal;
     std::time_t creationTime; // atributo para poder accesar al tiempo local en forma de fecha y hora.
@@ -23,9 +26,6 @@ class Menu
         // getters
         std::time_t getCreationTime();
 
-        // metodo para calcular el total de las calorias contenidas en el menu
-        void calculateTotalCal();
-
         // metodo de impresion
         void printMenu();
 };
@@ -37,11 +37,6 @@ Menu::Menu()
     creationTime = time(0);
 }
 
-void Menu::calculateTotalCal()
-{
-    
-}
-
 std::time_t Menu::getCreationTime()
 {
     return creationTime;
@@ -49,5 +44,52 @@ std::time_t Menu::getCreationTime()
 
 void Menu::printMenu()
 {
-    std::cout << "Creation date of Menu: " << asctime(localtime(&creationTime)) << std::endl;
+    foodCatalog.printGroupListFromFile("Foodlist.txt");
+    while (true)
+    {
+        // le preguntamos al usuario si quiere ver los contenidos de los grupos 
+        std::cout << "Do you wish to view the Food Groups' contents? [Y] Yes, [N] No: ";
+        char goption;
+        std::cin >> goption;
+        if (goption == 'Y' || goption == 'y')
+        {
+            // pedimos al metodo que nos muestre el contenido de cada grupo
+            foodCatalog.viewFoodGroupsContentFromList("Foodlist.txt");
+            std::ifstream inputFile("Foodlist.txt");
+            if (inputFile.is_open())
+            {
+                std::string line;
+                while(std::getline(inputFile, line))
+                {
+                    if (line.find("Calories: ") != std::string::npos)
+                    {
+                        // extraemos el valor que tenemos establecidos en cada alimento guardado en cada grupo
+                        std::string caloriesStr = line.substr(line.find("Calories: ") + 10); // 10 es la longitud de "Calories"
+                        double calories = std::stod(caloriesStr);
+
+                        // sumamos la cantidad total de calorias
+                        totalCal += calories;
+                    }
+                }
+                inputFile.close();
+            }
+
+            std::cout << "Total Calories: " << totalCal << "kcal" << std::endl;
+            break;
+        }
+        else if (goption == 'N' || goption == 'n')
+        {
+            // en caso de que el usuario no quiera verlo, se le mostrará directamente la cantidad de calorías totales en el catálogo
+            std::cout << "Total Calories: " << totalCal << "kcal" << std::endl;
+            break;
+        }
+        else if (goption != 'Y' || goption != 'y' && goption != 'N' || goption != 'n')
+        {
+            // si la respuesta no es 'Y' o 'N', vuelve a preguntar si quieres ver el contenido de los grupos 
+            std::cout << "Invalid option! Please enter [Y] for Yes or [N] for No" << std::endl;
+            continue;
+        }
+    }
+
+    std::cout << "\nCreation date of Menu: " << asctime(localtime(&creationTime)) << std::endl;
 }
